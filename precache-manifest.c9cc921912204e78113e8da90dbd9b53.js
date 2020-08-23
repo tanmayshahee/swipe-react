5e27c1018e60196a8555d0293cde3fc17bee4b3e
tanmayshahee <tanmayshahee@gmail.com>
2020-08-23 11:40:08 +0530

    Updates

---
+++ precache-manifest.c9cc921912204e78113e8da90dbd9b53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1e73426d37175966a908439317b8ae",
+    "url": "/swipe-react/index.html"
+  },
+  {
+    "revision": "605687ee23b3006104a1",
+    "url": "/swipe-react/static/css/2.24abdfb6.chunk.css"
+  },
+  {
+    "revision": "4d937f09473f96fa9013",
+    "url": "/swipe-react/static/css/main.5e39eefa.chunk.css"
+  },
+  {
+    "revision": "605687ee23b3006104a1",
+    "url": "/swipe-react/static/js/2.40238a43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/swipe-react/static/js/2.40238a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d937f09473f96fa9013",
+    "url": "/swipe-react/static/js/main.4110dd20.chunk.js"
+  },
+  {
+    "revision": "0a82dc9312f430e39aa3",
+    "url": "/swipe-react/static/js/runtime-main.b64d7935.js"
+  }
+]);
